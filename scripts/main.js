8c1ed8ab31d40c78a2668c4655c1012cea6744ba
Patrick Werner <pwerner2@gmail.com>
2015-01-27 19:19:53 -0500

    removed irrelevant code

--- scripts/main.js
+++ scripts/main.js
@@ -39,52 +39,9 @@
 
 
 
-// $(".sort-dropdown").change(function(sort) {
-//       if ($(".sort-dropdown option:selected").text() == "Lowest Price") {
-//         legos = _.sortBy(legos, function(data) {
-//           return +lego.price;
-//           });
-//       } else if ($(".sort-dropdown option:selected").text() == "Highest Price") {
-//         legos = _.sortBy(legos, function(data) {
-//           return +lego.price;
-//           }).reverse();
-//       }
-//       changeOrder(legos);
-//     });
-
-    // function changeOrder(data) {
-    //   $output.empty();
-    //   data.forEach(function(lego) {
-    //     var legoInfo = renderTemplate('lego-item', {
-    //       image: lego.Images[0].url_170x135,
-    //       title: lego.title,
-    //       titleURL: lego.url,
-    //       shopName: lego.Shop.shop_name,
-    //       price: lego.price,
-    //       currency: lego.currency_code
-    //     });
-    //     $output.append(legoInfo);
-    //   });
-    // }
-
-    //
-    // legos.forEach(function(lego){
-    //   var legoInfo = renderTemplate('lego-item', {
-    //     image: lego.Images[0].url_170x135,
-    //     title: lego.title,
-    //     titleURL: lego.url,
-    //     shopName: lego.Shop.shop_name,
-    //     price: lego.price,
-    //     currency: lego.currency_code
-    //   });
-    //   $output.append(legoInfo);
-    // });
 
   });
 
-/*
-  Templete function
-*/
   function renderTemplate(name, data) {
     var $template = $('[data-template-name=' + name + ']').text();
     $.each(data, function(prop, value) {
